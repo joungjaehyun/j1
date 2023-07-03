1d6809fbba30c5242458b781a21e0cf271a3998d
gwanyongpark <joungjaehyun1008@gmail.com>
2023-07-03 11:32:43 +0900

    entity및 repository 생성Crud

--- src/main/java/org/zerock/j1/domain/Sample.java
+++ src/main/java/org/zerock/j1/domain/Sample.java
@@ -1,5 +1,20 @@
 package org.zerock.j1.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_sample")
 public class Sample {
+    // Entity Class
+    // 모든 Entity는 primary key(ID)가있어야된다
+    @Id
+    private String keyCol;
+    
+    private String first;
+    
+    private String last;
     
 }
