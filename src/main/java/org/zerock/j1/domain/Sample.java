3777f9942e122d45a32223c10c2b9cf61137e2d0
gwanyongpark <joungjaehyun1008@gmail.com>
2023-07-03 12:26:52 +0900

    CRUD test 및 paging

--- src/main/java/org/zerock/j1/domain/Sample.java
+++ src/main/java/org/zerock/j1/domain/Sample.java
@@ -4,12 +4,23 @@ package org.zerock.j1.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tbl_sample")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
 public class Sample {
     // Entity Class
     // 모든 Entity는 primary key(ID)가있어야된다
+    // Entity 객체는 수정은 DB수정이니까 getter만
     @Id
     private String keyCol;
     
