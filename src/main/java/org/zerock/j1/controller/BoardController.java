a89407ba9dea7c98e34578113e2e9940b6faa0de
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-06 12:27:55 +0900

    CORS 처리

--- src/main/java/org/zerock/j1/controller/BoardController.java
+++ src/main/java/org/zerock/j1/controller/BoardController.java
@@ -2,6 +2,7 @@ package org.zerock.j1.controller;
 
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/api/board/")
 @RequiredArgsConstructor
 @Log4j2
+@CrossOrigin
 public class BoardController {
     
     private final BoardService boardService;
