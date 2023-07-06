b74c2b227e498d884179c919bd318f831bea378c
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-06 15:33:30 +0900

    PageNum 처리

--- src/main/java/org/zerock/j1/dto/PageResponseDTO.java
+++ src/main/java/org/zerock/j1/dto/PageResponseDTO.java
@@ -1,6 +1,7 @@
 package org.zerock.j1.dto;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import lombok.Data;
 
@@ -47,5 +48,8 @@ public class PageResponseDTO<E> {
         this.end = tempEnd > realEnd ? realEnd : tempEnd;
 
         this.next  = (this.end * this.size) < totalCount;
+
+        //pageNums 처리
+        this.pageNums = IntStream.rangeClosed(start, end).boxed().toList();
     }
 }
