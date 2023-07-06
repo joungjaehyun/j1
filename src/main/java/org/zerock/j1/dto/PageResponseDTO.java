085c129e17335fe9eb4fbe2215aae8f9dc890150
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-06 12:19:11 +0900

    Board list finish

--- src/main/java/org/zerock/j1/dto/PageResponseDTO.java
+++ src/main/java/org/zerock/j1/dto/PageResponseDTO.java
@@ -22,7 +22,7 @@ public class PageResponseDTO<E> {
     
     private PageRequestDTO requestDTO;
     
-    private int page, size;
+    private int page, size, start, end;
 
 
     public PageResponseDTO(List<E> dtoList, long totalCount, PageRequestDTO pageRequestDTO){
@@ -34,6 +34,18 @@ public class PageResponseDTO<E> {
         this.page = pageRequestDTO.getPage();
 
         this.size = pageRequestDTO.getSize();
-        
+
+        // 13
+        int tempEnd = (int)(Math.ceil(page/10.0) * 10);
+
+        this.start = tempEnd-9;
+        this.prev = this.start !=1;
+
+        //20 17.8
+        int realEnd =  (int)Math.ceil(totalCount/(double)size);
+
+        this.end = tempEnd > realEnd ? realEnd : tempEnd;
+
+        this.next  = (this.end * this.size) < totalCount;
     }
 }
